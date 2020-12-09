8a11086973f4f282d5d4738f6175f50cd5b454f1
Muhammad Noman <g00351754@gmit.ie>
2020-12-09 04:06:57 +0000

    generated spaceworld

---
+++ Endless Runner Game 2020/Assets/Scripts/GenerateWorld.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    public GameObject[] platforms ;
+
+    void Start()
+    {
+        Vector3 pos = new Vector3(0, 0, 0);
+        for (int i = 0; i < 20; i++)
+        {
+            int plateformNumber = Random.Range(0, platforms.Length);
+            GameObject p = Instantiate(platforms[plateformNumber], pos, Quaternion.identity);
+            
+
+            if (platforms[plateformNumber].tag == "stairsUp")
+            {
+                pos.y += 5;
+            }
+            if (platforms[plateformNumber].tag == "stairsDown")
+            {
+                pos.y -= 5;
+                p.transform.Rotate(new Vector3(0, 180, 0));
+                p.transform.position = pos;
+            }
+            pos.z -= 10;
+        } 
+    }
+}
+   
