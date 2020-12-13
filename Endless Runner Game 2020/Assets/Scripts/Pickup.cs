deb5c57be4f0164e05a0d31c7eb9c7b045a0687e
Muhammad Noman <g00351754@gmit.ie>
2020-12-13 04:06:22 +0000

    Add score on coin collection

---
+++ Endless Runner Game 2020/Assets/Scripts/Pickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+     void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+             GameData.singleton.UpdateScore(10);
+            Destroy(this.gameObject, 0.5f);
+           // Debug.Log("Pickup");
+        }
+    }
+}
---
+++ Endless Runner Game 2020/Assets/Scripts/RegisterScore.cs
@@ -0,0 +1,14 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class RegisterScore : MonoBehaviour
+{
+    void Start()
+    {
+         GameData.singleton.scoreText = this.GetComponent<Text>();
+    }
+
+    
+}
